705acbfd884e3dd62af2b96fa08589723a832cf2
ganialCheteu <cheteuganial@gmail.com>
2025-07-31 22:20:28 +0100

    fix:refactoring of checkIfUserIsSuperAdmin

--- app/Http/Middleware/CheckIfUserIsSuperAdmin.php
+++ app/Http/Middleware/CheckIfUserIsSuperAdmin.php
@@ -4,8 +4,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Session;
+use Symfony\Component\HttpFoundation\Response;
 
 class CheckIfUserIsSuperAdmin
 {
@@ -15,22 +15,19 @@ class CheckIfUserIsSuperAdmin
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response
-    { //verifie si le user connecte est super admin
-        if (Auth::check() && Auth::user()->role !== 'super_admin') {
-            // Auth::logout();
-            // return redirect()->route('login')->with('error', 'No permission to access this page.');
-
-            //deconnecte
-            Auth::logout();
-            //Rend la session invalide
-            Session::invalidate();
-            //regeneration du token crsf pour la securite(empeche l'ancien jeton crsf d'etre utilise)
-            Session::regenerateToken();
-            //affichage de la page d'erreur
-            abort(403,'Access Denied'); //declanche immediatement la vue 403
+    {
+        if (! Auth::check()) {
+            return $next($request);
         }
+
+        if (Auth::user()->role !== 'super_admin') {
+            Auth::logout();
+            Session::invalidate();
+            Session::regenerateToken();
+
+            abort(403, 'Access Denied');
+        }
+
         return $next($request);
     }
 }
-
-//ce middleware a ete enregistre dans le fichier bootstrap/app.php
